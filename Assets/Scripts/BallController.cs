ae73adf1e56c08d6bb88fe09b688a01f6ca5f0de
Alex <popov.alex.elf@gmail.com>
2018-05-07 22:13:45 +0300

    Created empty scripts for balls and sceneController

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -2,16 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SceneController : MonoBehaviour {
+public class BallController : MonoBehaviour {
 
+    private Vector2 position;
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
 	}
-   
+    
+    public void OnCollisionEnter2D(Collision2D col)
+    {
+
+    }
 }
