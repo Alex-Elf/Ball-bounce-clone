c1db424d4885529cd721deb5e7860ca30ef1c6e2
Alex <popov.alex.elf@gmail.com>
2018-05-09 00:36:25 +0300

    Start for spawning balls

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,17 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
-public class SceneController : MonoBehaviour {
 
+public class SceneController : MonoBehaviour {
+    public GameObject ball;
+
+    private GameObject Spawner;
+
+    private Text ui_text;
+    private int scores = 0;
 	// Use this for initialization
 	void Start () {
-		
+        ui_text = GameObject.Find("Score").GetComponent<Text>();
+        Spawner = GameObject.Find("Spawner");
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void FixedUpdate () {
+        ui_text.text = "Scores: " + scores;
+
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(
+                Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero,1000);
+
+
+            //Instantiate(ball, hit.point, new Quaternion(), transform);
+            //scores++;
+            Debug.Log(hit.point);
+        }
+
+    }
    
 }
