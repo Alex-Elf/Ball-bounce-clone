3650303686f8e965b37c911c3e78f4ef32ed6d5f
Alex <popov.alex.elf@gmail.com>
2018-05-09 15:54:57 +0300

    firing all balls and started scripting squares

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -6,36 +6,55 @@ using UnityEngine;
 
 public class SceneController : MonoBehaviour {
     public GameObject ball;
+    public float fireRate;
+    public int ballsMaxCount = 100;
 
     private GameObject Spawner;
 
     private Text ui_text;
-    private int scores = 0;
-    private Vector2 mouseScreenPosition;
+    private Vector2 mouseWorldPosition;
     private Vector2 direction;
+
+    private int ballsCurrentCount;
+    private int loadedBalls;
+    private bool firing = false;
+    private float timer = 0;
+
     // Use this for initialization
     void Start () {
         ui_text = GameObject.Find("Score").GetComponent<Text>();
         Spawner = GameObject.Find("Spawner");
+        ballsCurrentCount = ballsMaxCount;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-        ui_text.text = "Scores: " + scores;
-
-        if (Input.GetMouseButton(0))
+        timer += Time.deltaTime;
+        ui_text.text = "Balls: " + ballsCurrentCount + "\nMax Balls: " + ballsMaxCount;
+        if (firing && loadedBalls > 0 && timer >= fireRate)
+        {
+            var b = Instantiate(ball, Spawner.transform.position, new Quaternion(), transform);
+            b.GetComponent<Rigidbody2D>().AddForce(direction * 600);
+            timer = 0;
+            loadedBalls--;
+        }
+        if (!firing && Input.GetMouseButton(0))
         {
             //RaycastHit2D hit = Physics2D.Raycast(
             //    Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero,1000);
-            mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            direction = (mouseScreenPosition - (Vector2)Spawner.transform.position).normalized;
+
+            Spawner.transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = true;
+            mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            direction = (mouseWorldPosition - (Vector2)Spawner.transform.position).normalized;
             Spawner.transform.right = direction;
         }
-        if (Input.GetMouseButtonUp(0)) { 
-            var b = Instantiate(ball, Spawner.transform.position, new Quaternion(), transform);
-            b.GetComponent<Rigidbody2D>().AddForce(direction * 600);
+        if (!firing && Input.GetMouseButtonUp(0)) {
+            loadedBalls = ballsCurrentCount;
+            ballsCurrentCount = 0;
+            firing = true;
+            Spawner.transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = false;
             //scores++;
-            Debug.Log(mouseScreenPosition);
+            Debug.Log(mouseWorldPosition);
         }
 
     }
