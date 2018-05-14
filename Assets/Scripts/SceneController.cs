f7cb6e99e7602250c47078344963acd64053c755
Alex <popov.alex.elf@gmail.com>
2018-05-14 16:28:44 +0300

    Row spawning after shooting, gradient for squares

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class SceneController : MonoBehaviour {
     public GameObject ball;
     public GameObject square;
+    public GameObject rowPref;
     public float fireRate;
     public int ballsMaxCount = 100;
     public int ballsCurrentCount;
@@ -25,56 +26,30 @@ public class SceneController : MonoBehaviour {
 
     private List<GameObject> rows = new List<GameObject>();
     private int maxRowLength = 9;
-    private float squareWidth = 0.12f;
     private float startPosLeft = -0.44f;
     private float startPosTop = 0.44f;
     //private float width;
 
+    public Gradient grad;
+    private GradientColorKey[] gck = new GradientColorKey[7];
+    private GradientAlphaKey[] gak = new GradientAlphaKey[7];
+    public Color[] colors;
+
     void Start () {
+        colors = new Color[] {Color.blue, new Color(0,1,1),Color.green,
+                              new Color(1,1,0),Color.red,new Color(1,0,1),Color.blue};
+
+        SetGradient();
         ui_text = GameObject.Find("Score").GetComponent<Text>();
         Spawner = GameObject.Find("Spawner");
         ballsCurrentCount = ballsMaxCount;
         CreateNewRow();
     }
 
-    void CreateNewRow()
-    {
-
-        var r = Instantiate(new GameObject("row" + rows.Count), transform);
-        var r_pos = r.transform.localPosition;
-        r_pos.y = startPosTop;
-        r.transform.localPosition = r_pos;
-
-        var count = Random.Range(1, maxRowLength+1);
-        GameObject[] cells = new GameObject[maxRowLength];
-
-        while (count > 0)
-        {
-
-            var pos = Random.Range(0, maxRowLength);
-            if (cells[pos])
-            {
-                continue;
-            }
-            else
-            {
-                var sq = Instantiate(square, r.transform);
-                sq.transform.localPosition = new Vector2(startPosLeft + 0.11f * pos, 0);
-                count--;
-            }
-        }
-
-        foreach (GameObject row in rows){
-            var m = row.transform.position;
-            m.y += 0.11f;
-            row.transform.position = m;
-        }
-        rows.Add(r);
-
-    }
+    
 	void FixedUpdate () {
         //TODO: Fix firing start on buttonClick
-
+        
 
         timer += Time.deltaTime;
         ui_text.text = "Balls: " + ballsCurrentCount + 
@@ -93,6 +68,7 @@ public class SceneController : MonoBehaviour {
             bonus = 0;
             ballsCurrentCount = ballsMaxCount;
             firing = false;
+            CreateNewRow();
         }
         if (!firing && Input.GetMouseButton(0))
         {
@@ -109,8 +85,76 @@ public class SceneController : MonoBehaviour {
             ballsCurrentCount = 0;
             firing = true;
             Spawner.transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = false;
-            //scores++;
-            Debug.Log(mouseWorldPosition);
+            
+            //Debug.Log(mouseWorldPosition);
         }
     }
+    void LateUpdate()
+    {
+        var l = new List<GameObject>(rows);
+        foreach(GameObject row in rows)
+        {
+            if(!row ||row.transform.childCount == 0)
+            {
+                l.Remove(row);
+                Destroy(row);
+            }
+        }
+        rows = l;
+    }
+    void CreateNewRow()
+    {
+        var r = Instantiate(rowPref, transform);
+        r.name = "row" + rows.Count;
+        var r_pos = r.transform.localPosition;
+        r_pos.y = startPosTop;
+        r.transform.localPosition = r_pos;
+
+        var count = Random.Range(1, maxRowLength -1);
+        bool[] cells = new bool[maxRowLength-2];
+
+        while (count > 0)
+        {
+            var pos = Random.Range(0, maxRowLength-2);
+            if (cells[pos])
+            {
+                continue;
+            }
+            else
+            {
+                var sq = Instantiate(square, r.transform);
+                sq.transform.localPosition = new Vector2(startPosLeft + 0.11f * pos, 0);
+                sq.GetComponent<SquareController>().hits = Random.Range(ballsMaxCount/2, 
+                                                                    ballsMaxCount * 2);
+                sq.GetComponent<SquareController>().grad = grad;
+                cells[pos] = true;
+                count--;
+            }
+        }
+
+        foreach (GameObject row in rows)
+        {
+            var m = row.transform.localPosition;
+            m.y -= 0.11f;
+            row.transform.localPosition = m;
+        }
+        rows.Add(r);
+
+    }
+
+    void SetGradient()
+    {
+        for (int i = 0; i < gck.Length; i++)
+        {
+            gck[i].color = colors[i];
+            gck[i].time = (float)i / (gck.Length - 1);
+        }
+        for (int i = 0; i < gak.Length; i++)
+        {
+            gak[i].alpha = 1;
+            gak[i].time = (float)i / (gak.Length - 1);
+        }
+        grad.SetKeys(gck, gak);
+    }
+
 }
--- Assets/Scripts/SquareController.cs
+++ Assets/Scripts/SquareController.cs
@@ -7,15 +7,18 @@ public class SquareController : MonoBehaviour {
     public int hits;
     private SceneController scene_controller;
     private Text ui_Text;
-	void Start () {
+    public Gradient grad;
+
+    void Start () {
         ui_Text = GetComponentInChildren<Text>();
         scene_controller = GameObject.Find("Pannel").GetComponent<SceneController>();
-	}
+    }
 
     void FixedUpdate()
     {
         ui_Text.fontSize = (ui_Text.text.Length > 2) ? 50 : 70;
         ui_Text.text = hits.ToString();
+        //GetComponent<SpriteRenderer>().color = grad.Evaluate();
         if (hits <= 0) Destroy(gameObject);
     }
         
@@ -26,4 +29,7 @@ public class SquareController : MonoBehaviour {
         scene_controller.scores++;
         ui_Text.fontSize -= 10;
     }
+
+    
+
 }
